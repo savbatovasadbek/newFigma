c85821a2cdb5ea1fcec885625eb065460f363ffa
Hosilaxon <kamolovahosila@gmail.com>
2023-11-21 17:31:42 +0500

    Section added

---
+++ src/components/Trusted_by/Trusted.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../../App.css";
+import styles from "./Trusted.module.css";
+
+
+function Trusted() {
+  return (
+    <>
+    <h5>Trusted by</h5>
+    
+    </>
+  )
+}
+
+export default Trusted;
