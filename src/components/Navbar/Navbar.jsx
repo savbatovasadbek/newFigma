c2f724ab9fff98d82df37b0967491ebdb230058e
Sodiqova Marjona <marjonasodiqova63@gmail.com>
2023-11-21 16:16:05 +0300

    navbar add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <li><a href="#">Services</a></li>
             <li><a href="#">Projects</a></li>
             <li><a href="#">Blog</a></li>
-            <li><a href="#">Book a call
+            <li ><a href="#">Book a call
             <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" fill="none">
             <g clip-path="url(#clip0_1_452)">
               <path d="M9.72837 5.50978L5.57767 1.35907L6.67183 0.2649L12.6905 6.28359L6.67183 12.3023L5.57767 11.2081L9.72837 7.0574H0.30957V5.50978H9.72837Z" fill="black"/>
