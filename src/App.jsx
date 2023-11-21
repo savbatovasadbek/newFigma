e18efa1add662888fb536139d5498f8ecc7b844e
nilufarmirzayeva <programmer>
2023-11-21 18:04:13 +0500

    app.jsx testimonials added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Service from "./components/Service/Service";
+import Testimonials from "./components/Testimonials/Testimonials"
 
 function App() {
   return (
     <>
       <Navbar />
       <Service />
+      <Testimonials/>
     </>
   );
 }
